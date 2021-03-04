848680b2b0aa6638afa2c0ee0dff257312de9aa7
nass92 <goujanassim@hotmail.com>
2021-03-04 15:57:18 +0100

    ewe

--- stringUtils.js
+++ stringUtils.js
@@ -3,6 +3,14 @@ const reverseStr = (str) => {
   return str.split("").reverse().join("");
 }
 
-console.log(reverseStr("kayak"))
+console.log(reverseStr("hello!"))
 
 // deuxieme partie: pas trouvée encore la solution
+const isPalindrome = (str) => {
+  if (reverseStr(str) === str) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log(isPalindrome("kayak"))
